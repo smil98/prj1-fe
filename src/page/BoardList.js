8b9357f32f00c12454fc9903e763a671cf5b80f5
smil98 <seungmilee98@gmail.com>
2023-11-09 14:52:38 +0900

    Added Styles for BoardList

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,5 +1,40 @@
+import { Box, Table, Thead, Tr, Th, Td, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/board/list")
+      .then((response) => setBoardList(response.data));
+  }, []);
+
   return (
-    <h1>Read Boards</h1>
+    <Box>
+      <h1>Read Boards</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Title</Th>
+              <Th>by</Th>
+              <Th>Date</Th>
+            </Tr>
+          </Thead>
+        </Table>
+        {boardList || <Spinner />}
+        {boardList &&
+          boardList.map((board) => (
+            <Tr>
+              <Td>{board.id}</Td>
+              <Td>{board.title}</Td>
+              <Td>{board.writer}</Td>
+              <Td>{board.inserted}</Td>
+            </Tr>
+          ))}
+      </Box>
+    </Box>
   );
 }
