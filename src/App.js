fe7956b270a4930b3008027b69f9922a1df0100e
smil98 <seungmilee98@gmail.com>
2023-11-09 10:30:45 +0900

    Created HomeLayout, BoardList, BoardWrite

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {HomeLayout} from "./layout/HomeLayout";
+import {BoardList} from "./page/BoardList";
+import {BoardWrite} from "./page/BoardWrite";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route index element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
+    </Route>
+  )
+)
 
 function App(props) {
   return (
