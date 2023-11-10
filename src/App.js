d378ea139d047a4b62c694f30519876f46b36054
smil98 <seungmilee98@gmail.com>
2023-11-10 16:21:15 +0900

    Added MemberList page where all member info is loaded

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BoardWrite } from "./page/board/BoardWrite";
 import { BoardView } from "./page/board/BoardView";
 import { BoardEdit } from "./page/board/BoardEdit";
 import { MemeberSignUp } from "./page/member/MemeberSignUp";
+import { MemberList } from "./page/member/MemberList";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +21,7 @@ const routes = createBrowserRouter(
       <Route path="board/:id" element={<BoardView />} />
       <Route path="edit/:id" element={<BoardEdit />} />
       <Route path="join" element={<MemeberSignUp />} />
+      <Route path="member/list" element={<MemberList />} />
     </Route>,
   ),
 );
