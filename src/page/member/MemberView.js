f68c793821e8ddc31c8209d4a7cb54d3c8966864
smil98 <seungmilee98@gmail.com>
2023-11-13 09:39:16 +0900

    Added FormControl to Member View Page

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,12 +1,45 @@
 import { useSearchParams } from "react-router-dom";
-import { Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Spinner,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function MemberView() {
+  const [member, setMember] = useState(null);
   const [params] = useSearchParams();
+  useEffect(() => {
+    axios
+      .get("/api/member?" + params.toString())
+      .then((response) => setMember(response.data))
+      .catch(() => console.log("error"));
+  }, []);
+
+  if (member === null) {
+    return <Spinner />;
+  }
 
   return (
-    <div>
+    <Box>
       <Heading>{params.get("id")} Account Info</Heading>
-    </div>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input type="text" value={member.email} readOnly />
+      </FormControl>
+      <FormControl>
+        <Button colorScheme="purple">Edit</Button>
+        <Button colorScheme="red">Delete</Button>
+      </FormControl>
+    </Box>
   );
 }
