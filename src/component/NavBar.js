11b908d6868137a863f0a8753a5c0c2d33c50d63
smil98 <seungmilee98@gmail.com>
2023-11-14 09:51:59 +0900

    Added Logout function

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,16 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function NavBar() {
   const navigate = useNavigate();
+
+  function handleLogout() {
+    axios
+      .post("/api/member/logout")
+      .then(() => console.log("logout successful"));
+  }
+
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>Home</Button>
@@ -10,6 +18,7 @@ export function NavBar() {
       <Button onClick={() => navigate("/join")}>Sign Up</Button>
       <Button onClick={() => navigate("/member/list")}>Member List</Button>
       <Button onClick={() => navigate("/login")}>Log in</Button>
+      <Button onClick={handleLogout}>Log out</Button>
     </Flex>
   );
 }
